d7b15d48b51603f193269f387a3c3126488c008e
haiyu.song <haiyu.song@item.com>
2026-02-11 11:17:30 +0800

    编码规范

--- backend/console/src/main/java/com/alibaba/higress/console/controller/NacosManagementController.java
+++ backend/console/src/main/java/com/alibaba/higress/console/controller/NacosManagementController.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright (c) 2022-2026 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.alibaba.higress.console.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.higress.console.controller.util.ControllerUtil;
 import com.alibaba.nacos.maintainer.client.config.ConfigMaintainerService;
